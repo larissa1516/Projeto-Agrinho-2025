b6bdbec4176c59d9e9a31d8f29ad3fc6d1239cdc
larissa1516 <larissa.sampaio.souza@escola.pr.gov.br>
2025-05-30 15:04:35 -0300

    .

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    const toggleButton = document.getElementById('toggleButton');
-    const conteudoOculto = document.getElementById('conteudo-oculto');
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js"></script>
+    <script src="script.js"></script>
+  
+</body>
 
-    
-    toggleButton.addEventListener('click', function() {
-        
-        const isHidden = conteudoOculto.classList.contains('oculto');
-
-        
-        if (isHidden) {
-            conteudoOculto.classList.remove('oculto'); 
-            toggleButton.textContent = 'Esconder Conteúdo Adicional'; 
-            toggleButton.setAttribute('aria-expanded', 'true'); 
-        } else {
-            conteudoOculto.classList.add('oculto'); 
-            toggleButton.textContent = 'Mostrar Conteúdo Adicional'; 
-            toggleButton.setAttribute('aria-expanded', 'false'); 
-        }
-
-    });
-});
-
+</html>
