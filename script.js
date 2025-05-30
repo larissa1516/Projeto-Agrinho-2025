16d25779cfabd629ddbf9e65f51e44dc1b7c8e76
larissa1516 <larissa.sampaio.souza@escola.pr.gov.br>
2025-05-30 15:34:38 -0300

    .

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded',<AnimationTimelinefunction(){
-
-ScrollReveal().reveal("introducao", { delay: 500 });
- ScrollReveal().reveal("galeria", { delay: 500 });
- ScrollReveal().reveal("texto-explicativo", { delay: 500 });
