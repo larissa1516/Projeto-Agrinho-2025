c23e350bf362a92c169627273c339e99ffdc22c1
larissa1516 <larissa.sampaio.souza@escola.pr.gov.br>
2025-05-30 14:49:04 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.addEventListener(DOMcontentLoaded',function(){
-    }
-ScrollReveal().reveal('#inicio', { delay: 500 });
-ScrollReveal().reveal('#Festejando a Interação entre Campo e Cidade', { delay: 500 });
-ScrollReveal().reveal('#galeria', { delay: 500 });
-ScrollReveal().reveal('#Pontes de Conexão', { delay: 500 });
+
